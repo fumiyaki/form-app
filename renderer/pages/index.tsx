ffff60ec99061bc9e5180fb32b3050849f47e0fe
yono fumiya <z2ab8q3ty0@gmail.com>
2022-05-21 18:21:06 +0900

    baseの準備

--- renderer/pages/index.tsx
+++ renderer/pages/index.tsx
@@ -1,34 +1,78 @@
-import { useEffect } from 'react'
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import { useEffect, useState } from "react";
+import Layout from "../components/Layout";
 
 const IndexPage = () => {
+  const [isFormDone, setIsFormDone] = useState(false);
   useEffect(() => {
-    const handleMessage = (_event, args) => alert(args)
+    const handleMessage = (_event, args) => alert(args);
 
     // add a listener to 'message' channel
-    global.ipcRenderer.addListener('message', handleMessage)
+    global.ipcRenderer.addListener("message", handleMessage);
 
     return () => {
-      global.ipcRenderer.removeListener('message', handleMessage)
-    }
-  }, [])
-
-  const onSayHiClick = () => {
-    global.ipcRenderer.send('message', 'hi from next')
-  }
+      global.ipcRenderer.removeListener("message", handleMessage);
+    };
+  }, []);
 
   return (
-    <Layout title="Home | Next.js + TypeScript + Electron Example">
-      <h1>Hello Next.js 👋</h1>
-      <button onClick={onSayHiClick}>Say hi to electron</button>
-      <p>
-        <Link href="/about">
-          <a>About</a>
-        </Link>
-      </p>
-    </Layout>
-  )
-}
+    <Layout>
+      {!isFormDone ? (
+        <>not dane</>
+      ) : (
+        <>
+          <div>
+            <div>氏名</div>
+            <div>
+              <input type="text" placeholder="code4biz" />
+              <input type="text" placeholder="太郎" />
+            </div>
+          </div>
+          <div>
+            <div>面接フェーズ</div>
+            <div>
+              <div>
+                <input
+                  type="radio"
+                  id="huey"
+                  name="drone"
+                  value="huey"
+                  checked
+                />
+                <label>Huey</label>
+              </div>
 
-export default IndexPage
+              <div>
+                <input type="radio" id="dewey" name="drone" value="dewey" />
+                <label>Dewey</label>
+              </div>
+
+              <div>
+                <input type="radio" id="louie" name="drone" value="louie" />
+                <label>Louie</label>
+              </div>
+            </div>
+            <div>
+              <div>面接メモ</div>
+              <textarea
+                name="memo"
+                cols={30}
+                rows={10}
+                placeholder="type here..."
+              />
+            </div>
+          </div>
+          <div>
+            <div>面接日時</div>
+            <input type="date" name="example" required />
+          </div>
+        </>
+      )}
+      <h1 style={{ color: "red" }}>Hello Next.js & Electron 👋</h1>
+      <button onClick={() => setIsFormDone((isForm) => !isForm)}>
+        Say hi to electron
+      </button>
+    </Layout>
+  );
+};
+
+export default IndexPage;
